cccf4cee9e4aee1eb8890e791c3f1ceaf9d28395
Michele Martone <michelemartone@users.sourceforge.net>
2020-05-11 22:05:26 +0200

    include librsb.pxd

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 from numpy import get_include
 include_dirs = [get_include()]
 
-setuptools.setup(
+setup(
     #name="rsb-michele.martone", version = '0.2.202005072047',
     name="rsb", version = '0.2.202005072047',
     #name="rsb", version = '0.2',
@@ -19,17 +19,17 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/michelemartone/pyrsb",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(), # rsb
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    ext_modules = [ Extension("rsb", ["rsb.pyx"], libraries=["rsb","z","hwloc","gfortran"], include_dirs = include_dirs ) ],
+    ext_modules = [ Extension("rsb", ["rsb.pyx","librsb.pxd"], libraries=["rsb","z","hwloc","gfortran"], include_dirs = include_dirs ) ],
     setup_requires = ['numpy', 'scipy'],
     install_requires = ['numpy', 'scipy'],
     cmdclass = {'build_ext': build_ext},
-    package_data = { 'rsb': ['rsb.pyx','librsb.pxd','*.py'] },
+    #package_data = { '': ['rsb.pyx','*.py'] },
     include_package_data = True,
     python_requires='>=3.7',
 )
