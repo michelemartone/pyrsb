39b5e931775ee3fc3a872b9c04a2fca22b340df2
Michele Martone <michelemartone@users.sourceforge.net>
2020-05-18 12:10:43 +0200

    reformat code invoking black --py36

--- setup.py
+++ setup.py
@@ -7,29 +7,38 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 from numpy import get_include
+
 include_dirs = [get_include()]
 
 setup(
-    #name="rsb-michele.martone", version = '0.2.202005072047',
-    name="rsb", version = '0.2.202005072047',
-    #name="rsb", version = '0.2',
+    # name="rsb-michele.martone", version = '0.2.202005072047',
+    name="rsb",
+    version="0.2.202005072047",
+    # name="rsb", version = '0.2',
     author="Michele Martone",
     author_email="michelemartone@users.sourceforge.net",
     description="PyRSB: a Cython-based Python interface to librsb",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/michelemartone/pyrsb",
-    packages=setuptools.find_packages(), # rsb
+    packages=setuptools.find_packages(),  # rsb
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
-    ext_modules = [ Extension("rsb", ["rsb.pyx","librsb.pxd"], libraries=["rsb","z","hwloc","gfortran"], include_dirs = include_dirs ) ],
-    setup_requires = ['numpy', 'scipy'],
-    install_requires = ['numpy', 'scipy'],
-    cmdclass = {'build_ext': build_ext},
-    #package_data = { '': ['rsb.pyx','*.py'] },
-    include_package_data = True,
-    python_requires='>=3.7',
+    ext_modules=[
+        Extension(
+            "rsb",
+            ["rsb.pyx", "librsb.pxd"],
+            libraries=["rsb", "z", "hwloc", "gfortran"],
+            include_dirs=include_dirs,
+        )
+    ],
+    setup_requires=["numpy", "scipy"],
+    install_requires=["numpy", "scipy"],
+    cmdclass={"build_ext": build_ext},
+    # package_data = { '': ['rsb.pyx','*.py'] },
+    include_package_data=True,
+    python_requires=">=3.7",
 )
