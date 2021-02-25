1fd33088dcacd4c1ebd26ae306a892ead0894aca
Michele Martone <michelemartone@users.sourceforge.net>
2021-02-25 10:49:44 +0100

    pin down another case worth reviewing

--- test/test_pyrsb.py
+++ test/test_pyrsb.py
@@ -103,6 +103,14 @@ def test_init_tuples_to_fix_2():
     assert ( mat.nnz() == 3 )
 
 
+def test_init_tuple_to_fix_3():
+    # TODO: shall align rsb_matrix to scipy's
+    mat = rsb_matrix(([1.,1.], ([-1,-2], [-1,-2])))
+    assert mat.shape == (0, 0)
+    assert mat.nnz() == 0
+    assert mat._is_unsymmetric() == True
+
+
 def test_do_print():
     [V,I,J,nr,nc,nnz] = gen_tri();
     rmat = rsb_matrix((V, (I, J)),[nr,nc])
