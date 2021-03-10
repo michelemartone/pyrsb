4bf39615ca8cec1dc3f96134a8a654ba2db5ed20
Michele Martone <michelemartone@users.sourceforge.net>
2021-03-10 20:45:18 +0100

    nrhs as yet another parameter

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -8,10 +8,9 @@ from pyrsb import rsb_matrix
 from pyrsb import _print_vec, rsb_time, _err_check, rsb_dtype, _dt2dt
 prv_t = rsb_dtype
 
-def gen_dense(dtype,order):
+def gen_dense(dtype,order,nrhs):
     nr = 1000
     nc = nr
-    nrhs = 4
     d = numpy.ones(shape=(nr,nc), dtype=dtype)
     x = numpy.ones([nc, nrhs], dtype=dtype, order=order)
     y = numpy.ones([nr, nrhs], dtype=dtype, order=order)
@@ -20,21 +19,21 @@ def gen_dense(dtype,order):
 dtypes = [numpy.float64, numpy.float32, numpy.complex128, numpy.complex64]
 
 class BenchDense():
-    params = [ dtypes, ['C','F'], ['csr','rsb'] ]
-    param_names = ["dtype","order","format"]
-    def setup(self,dtype,order,format):
-        [self.y,self.d,self.x] = gen_dense(dtype,order)
+    params = [ dtypes, [1, 2, 4, 8], ['C','F'], ['csr','rsb'] ]
+    param_names = ["dtype","nrhs","order","format"]
+    def setup(self,dtype,nrhs,order,format):
+        [self.y,self.d,self.x] = gen_dense(dtype,order,nrhs)
         if format == 'rsb':
             self.a = rsb_matrix(self.d)
         else:
             self.a = csr_matrix(self.d)
 
-    def time_bare(self,dtype,order,format):
+    def time_bare(self,dtype,nrhs,order,format):
         if format == 'rsb':
             self.a._spmm(self.x,self.y)
         else:
             scipy.sparse._sparsetools.csr_matvecs(self.a.shape[0], self.a.shape[1], self.x.shape[1], self.a.indptr, self.a.indices, self.a.data, self.x.ravel(), self.y.ravel())
 
-    def time_rsb(self,dtype,order,format):
+    def time_mul(self,dtype,nrhs,order,format):
         y = self.a * self.x
 
